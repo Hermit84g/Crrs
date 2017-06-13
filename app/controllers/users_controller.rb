d3f7dad62507e4e96993bb5b553ad17af3bb7449
Unknown <hermit84g@gmail.com>
2017-06-13 18:50:49 +0900

    20170613_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:home, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
 
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   end 
   
   def edit
-    @user = User.find_by(params[:Uid])
+    @user = User.find(params[:id])
   end
 
   def show
